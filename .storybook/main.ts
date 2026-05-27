d8d2bbf6cedc017886cdc653eee8f8f2b63d2cbd
Gerard Gajda <gerard.gajda@sleipnir.pl>
2026-05-27 18:38:18 +0200

    Dependencies upgrade & cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,10 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/react-vite/dist";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
   ],
